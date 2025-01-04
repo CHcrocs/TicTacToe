0f6f832fa87b852b0005bb3d5577671c5692147b
CHcrocs <138597970+CHcrocs@users.noreply.github.com>
2025-01-04 09:12:00 -0300

    back from the dead

--- tictactoe.js
+++ tictactoe.js
@@ -1,13 +1,26 @@
 function Gameboard() {
-    const gameboard = []
+    const gameboard = new Array(9).fill(null)     
+
+    this.getBoard = () => gameboard
     
+    this.setCell = (index, player) => {
+        if (gameboard[index] === null) {
+            gameboard[index] = player
+            return true
+        }
+        return false
+    }
+
+    this.resetBoard = () => {
+        gameboard.fill(null)
+    }
 }   
 
-function Player(marker) {
+function Player( name, marker) {
+    this.name = name
     this.marker = marker
-    
 }
 
-function GameState() {
+function GameController() {
 
 }
